03699c3a350e5e227cf0a8d33903e2d9883e5031
nasko25 <atanas.pashov@yahoo.com>
2021-10-10 20:08:09 +0200

    added additional expect statements to the unit test

--- __tests__/mappings/index.test.ts
+++ __tests__/mappings/index.test.ts
@@ -11,8 +11,20 @@ jest.mock("@polkadot/util");
 jest.mock("../../chain", () => jest.fn());
 
 test("test systemRemark() v0.1", () => {
-    const extrinsic = { args: [ { value: "0x1337" } ] } as unknown as SubstrateExtrinsic;
+    const value = "0x1337";
+    const extrinsic = { args: [ { value: value } ] } as unknown as SubstrateExtrinsic;
 
+    let spyToString = jest.spyOn(String.prototype, "toString");
+    let spyStartsWith = jest.spyOn(String.prototype, "startsWith");
     systemRemark({store, event, block, extrinsic});
     expect(store).toHaveBeenCalledTimes(0);
+    expect(spyToString).toHaveBeenCalledTimes(2);
+    expect(spyStartsWith).toHaveBeenCalledTimes(2);
+    expect(spyStartsWith).toHaveBeenNthCalledWith(1, "0x726d726b");
+    expect(spyStartsWith).toHaveBeenNthCalledWith(2, "0x524d524c");
+    expect(spyToString.mock.instances[0]).toBe(value);      // or toEqual() ?
+    expect(spyToString.mock.instances[1]).toBe(value);      // or toEqual() ?
+
+    spyToString.mockRestore();
+    spyStartsWith.mockRestore();
 });
