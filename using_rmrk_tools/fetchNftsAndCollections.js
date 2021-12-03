64f98d73825b1fae6089fab9bef3232a7a6c18a1
nasko25 <apashov0@gmail.com>
2021-12-03 19:17:20 +0100

    started writing a 'fetch nfts and collections' module

--- using_rmrk_tools/fetchNftsAndCollections.js
+++ using_rmrk_tools/fetchNftsAndCollections.js
@@ -1,42 +1,31 @@
-const { Consolidator } = require('rmrk-tools')
-const fetch = require('node-fetch');
+import { Nft } from '../generated/model/index.ts';
+import fetch from 'node-fetch';
 
 const WAIT_BETWEEN_FETCHES = 2 * 1000;     // how long to wait between fetches of rmrks from the database to not overload the db with requests
-const consolidator = new Consolidator();
 let lastRetrievedBlock = 0;
 
 
-async function fetchRmrkEntities() {
+async function fetchNftsAndCollections() {
     await fetch('http://localhost:4000/graphql', {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
-        body: JSON.stringify({query: `{ rmrkEntities (where: {block_gte: "${lastRetrievedBlock}"}, orderBy: block_DESC) { id block rmrk { caller interactionType remark rmrkVersion extraEx { call caller value } } } }`})
+        // body: JSON.stringify({query: `{ rmrkEntities (where: {block_gte: "${lastRetrievedBlock}"}, orderBy: block_DESC) { id block rmrk { caller interactionType remark rmrkVersion extraEx { call caller value } } } }`})
+        body: JSON.stringify({query: `{ nfts (where: {block_gte: "${lastRetrievedBlock}"}, orderBy: block_DESC) { block collection id metadata sn symbol transferable version } }`})
     })
         .then(res => res.json())
         .then(async data => {
+            // TODO
             console.log("data: ", JSON.stringify(data))
             // if rmrk entities are not empty
-            if (data.data.rmrkEntities.length > 0) {
+            if (data && data.data && data.data.nfts && data.data.nfts.length > 0) {
                 // get the biggest block you have retrieved
                 // (which is the first rmrk in data, as they are ordered by ascending block number)
-                lastRetrievedBlock = data.data.rmrkEntities[0].block;
+                lastRetrievedBlock = data.data.nfts[0].block;
 
-                const { nfts, collections } = await consolidator.consolidate(data.data.rmrkEntities.map(rmrk => {
-                    return {
-                        block: rmrk.block,
-                        caller: rmrk.rmrk.caller,
-                        interaction_type: rmrk.rmrk.interactionType,
-                        version: rmrk.rmrk.rmrkVersion,
-                        remark: rmrk.rmrk.remark,
-                        extra_ex: rmrk.rmrk.extraEx
-                    };
-                }));
                 console.log(nfts, collections);
-                // TODO save nfts and collections in the database
-                // TODO pass dbAdapter to Consolidator()
             }
         });
 }
@@ -50,17 +39,17 @@ process.on ('SIGINT',() => {
     process.exit(1);
 });
 
-async function fetchAllRmrkEntities() {
+async function fetchAllNftsAndCollections() {
     while (true) {
         console.log("Fetching rmrk entities from the database...");
-        await fetchRmrkEntities();
+        await fetchNftsAndCollections();
         console.log("Fetching done\nWaiting before fetching the next batch...");
         await sleep(WAIT_BETWEEN_FETCHES);
         console.log(lastRetrievedBlock)
     }
 }
 
-fetchAllRmrkEntities().then();
+fetchAllNftsAndCollections().then();
 
 //const { nfts, collections } = await consolidator.consolidate(remarks);
 //
