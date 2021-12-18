8d9e5e9b2dd970083b3a13217f81613425f9baa7
nasko25 <apashov0@gmail.com>
2021-12-18 17:26:26 +0100

    new tables in the rarity_rmrk db

--- db/sql/rarity.sql
+++ db/sql/rarity.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE rarity_rmrk;
 GRANT ALL PRIVILEGES ON DATABASE rarity_rmrk TO postgres;
 \connect rarity_rmrk;
-CREATE TABLE rarities ( id VARCHAR (200) PRIMARY KEY, rarity REAL );
+CREATE TABLE nfts ( nft_id VARCHAR (200) PRIMARY KEY, collection_id VARCHAR (200) );
+CREATE TABLE rarities ( id VARCHAR (200) PRIMARY KEY, rarity REAL, CONSTRAINT nft FOREIGN KEY (id) REFERENCES nfts(nft_id) ON DELETE CASCADE ON UPDATE CASCADE );
+CREATE TABLE metadatas ( id VARCHAR (200) PRIMARY KEY, metadata_json TEXT /* later may have other fields, like images from the metadata */, CONSTRAINT nft FOREIGN KEY (id) REFERENCES nfts(nft_id) ON DELETE CASCADE ON UPDATE CASCADE );
