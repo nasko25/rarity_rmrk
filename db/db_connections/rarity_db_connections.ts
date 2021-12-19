6daed4228f3ac25080aa9834d8b30ca3ec94cc08
nasko25 <apashov0@gmail.com>
2021-12-19 23:03:04 +0100

    added addMetdata() and addRarity() db connection functions

--- db/db_connections/rarity_db_connections.ts
+++ db/db_connections/rarity_db_connections.ts
@@ -4,7 +4,7 @@ export async function addNft(nft: Nft, DB_POOL) {
     // "INSERT ... ON CONFLICT DO NOTHING/UPDATE"
     // https://www.thisdot.co/blog/connecting-to-postgresql-with-node-js
     const query = `
-        INSERT INTO nfts (nft_id, collection_id) 
+        INSERT INTO nfts (nft_id, collection_id)
         VALUES ($1, $2)
         ON CONFLICT (nft_id) DO UPDATE SET collection_id = EXCLUDED.collection_id
         RETURNING nft_id
@@ -14,4 +14,27 @@ export async function addNft(nft: Nft, DB_POOL) {
     return DB_POOL.query(query, values);
 }
 
-// TODO addMetadata(), addRarity(), getMetadata(), getRarity(), getNftCollection()
+// TODO getMetadata(), getRarity(), getNftCollection()
+export async function addMetadata(nft_id: string, metadata: string, DB_POOL) {
+    const query = `
+        INSERT INTO metadatas (id, metadata_json)
+        VALUES ($1. $2)
+        ON CONFLICT (id) DO UPDATE SET metadata_json = EXCLUDED.metadata_json
+        RETURNING id
+    `;
+    const values = [ nft_id, metadata ];
+
+    return DB_POOL.query(query, values);
+}
+
+export async function addRarity(nft_id: string, rarity: number, DB_POOL) {
+    const query = `
+        INSERT INTO rarities (id, rarity)
+        VALUES ($1, $2)
+        ON CONFLICT (id) DO UPDATE SET rarity = EXCLUDED.rarity
+        RETURNING id
+    `;
+    const values = [ nft_id, rarity ];
+
+    return DB_POOL.query(query, values);
+}
