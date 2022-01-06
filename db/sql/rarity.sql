f3cc1e018ec961693be1ba19b2fa64949808a68d
nasko25 <apashov0@gmail.com>
2022-01-06 10:48:19 +0200

    removed comments and no longer used code

--- db/sql/rarity.sql
+++ db/sql/rarity.sql
@@ -2,7 +2,7 @@ CREATE DATABASE rarity_rmrk;
 GRANT ALL PRIVILEGES ON DATABASE rarity_rmrk TO postgres;
 \connect rarity_rmrk;
 CREATE TABLE nfts ( nft_id VARCHAR (800) PRIMARY KEY, collection_id VARCHAR (200) );
-CREATE TABLE rarities ( id VARCHAR (200) PRIMARY KEY, rarity REAL, CONSTRAINT nft FOREIGN KEY (id) REFERENCES nfts(nft_id) ON DELETE CASCADE ON UPDATE CASCADE );
+CREATE TABLE rarities ( id VARCHAR (800) PRIMARY KEY, rarity REAL, CONSTRAINT nft FOREIGN KEY (id) REFERENCES nfts(nft_id) ON DELETE CASCADE ON UPDATE CASCADE );
 CREATE TABLE metadatas ( id VARCHAR (800) PRIMARY KEY, metadata_json TEXT /* later may have other fields, like images from the metadata */, CONSTRAINT nft FOREIGN KEY (id) REFERENCES nfts(nft_id) ON DELETE CASCADE ON UPDATE CASCADE );
 CREATE TABLE metadata_information ( last_retrieved_block BIGINT );
 /* initially set the last_retrieved_block to 0 */
